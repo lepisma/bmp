2d2fba7ed690b0b05ecf74649b6c4237993d5de9
Abhinav Tushar <lepisma@fastmail.com>
2018-11-05 01:48:43 +0530

    0.2.4

--- bmp.el
+++ bmp.el
@@ -3,7 +3,7 @@
 ;; Copyright (c) 2018 Abhinav Tushar
 
 ;; Author: Abhinav Tushar <lepisma@fastmail.com>
-;; Version: 0.2.3
+;; Version: 0.2.4
 ;; Package-Requires: ((emacs "25"))
 ;; URL: https://github.com/lepisma/bmp
 
